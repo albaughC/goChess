799b6b7c4b1a5fedc4504558d1095f7c41b9ef74
albaughC <albaugh.claus@protonmail.com>
2020-07-12 20:23:00 +0000

    The real commit

--- private/html/userlist.js
+++ private/html/userlist.js
@@ -1,4 +1,4 @@
-const ws = new WebSocket("ws://chesslife.herokuapp.com/public/api/userwebsocket");
+const ws = new WebSocket("wss://chesslife.herokuapp.com/public/api/userwebsocket");
 
 ws.addEventListener("open", () => {
     console.log('We are connected');
